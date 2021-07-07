26a174bfbaccf22e5f2e9cdb97dd3d5188f30335
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-07 10:16:21 +0600

    Full CRUD operation

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -21,5 +21,65 @@ namespace InAndOut.Controllers
             IEnumerable<Item> objectList = _db.Items;
             return View(objectList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+           if(ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Items.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Item obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int ? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
